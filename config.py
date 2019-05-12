f6073385ce33fe206c3e41a80eb5e98393d7bd6c
spartan07 <arpanvenugopal2019@u.northwestern.edu>
2019-05-11 19:01:55 -0500

    Script to pre-process data and run file

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 DEBUG = True
 LOGGING_CONFIG = "config/logging/local.conf"
-PORT = 3000
-APP_NAME = "penny-lane"
-SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/tracks.db'
+PORT = 9038
+APP_NAME = "Football_Manager"
+SQLALCHEMY_DATABASE_URI = 'sqlite:///../data/user_reqs.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 HOST = "127.0.0.1"
